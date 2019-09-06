6041c408bf6683456214a4694f94ea654b2121b1
oleg <asicscwcs@gmail.com>
2019-09-06 23:41:41 +0300

    jdbc teaching

--- src/JDBCTeaching/DBTest.java
+++ src/JDBCTeaching/DBTest.java
@@ -1,7 +1,33 @@
 package JDBCTeaching;
 
+import java.sql.*;
+
 public class DBTest {
-    String login = "root";
-    String password = "edcvfr12!";
-    String url = "http//";
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+
+       Class.forName("org.postgresql.Driver");
+
+      final String user = "fluidbusiness";
+        final  String password = "Edcvfr12!";
+        final String url = "jdbc:postgresql://localhost:5432/fluidbusiness";
+
+        final Connection connect = DriverManager.getConnection(url, user, password);
+
+        try (PreparedStatement statement = connect.prepareStatement("SELECT * FROM users WHERE id = (?)")) {
+
+           statement.setInt(1,1);
+            final ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                String name = "name" + resultSet.getString("user_name");
+                System.out.println(name);
+            }
+        }
+        finally {
+            connect.close();
+
+        }
+
+    }
 }
+
