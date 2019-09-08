9ddd811f1445f87b82dab51040f893b1e771077a
oleg <asicscwcs@gmail.com>
2019-09-08 11:22:44 +0300

    Рабочее подключени к бд

--- src/JDBCTeaching/DBTest.java
+++ src/JDBCTeaching/DBTest.java
@@ -1,33 +1,55 @@
 package JDBCTeaching;
-
 import java.sql.*;
 
 public class DBTest {
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        try {
+            Class.forName("org.postgresql.Driver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Нет драйвера postgresql" + e);
+        }
 
-       Class.forName("org.postgresql.Driver");
+        System.out.println("Драйвер postgresql подключен");
 
       final String user = "fluidbusiness";
         final  String password = "Edcvfr12!";
-        final String url = "jdbc:postgresql://localhost:5432/fluidbusiness";
+        final String url = "jdbc:postgresql://94.103.90.9:5432/fluidbusiness";
+        Connection connect = null;
 
-        final Connection connect = DriverManager.getConnection(url, user, password);
+        try {
+    connect = DriverManager.getConnection(url, user, password);
 
-        try (PreparedStatement statement = connect.prepareStatement("SELECT * FROM users WHERE id = (?)")) {
+    } catch (SQLException e ) {
+    System.out.println("Нет подключения к БД" + e);
+    }
 
-           statement.setInt(1,1);
-            final ResultSet resultSet = statement.executeQuery();
+    if(connect != null )    System.out.println("Подключились к базе данных");
+        else System.out.println("Не удалось подключиться к БД");
+
+
+        //try (PreparedStatement statement = connect.prepareStatement("SELECT * FROM forwarders WHERE id = (?)")) {
+        try {
+            Statement statement = connect.createStatement();
+
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM users");
 
             if (resultSet.next()) {
-                String name = "name" + resultSet.getString("user_name");
+               String name =  resultSet.getString("user_name");
                 System.out.println(name);
+                //System.out.println("dsd");
+            } else {
+                System.out.println("Не зашло в иф");
             }
         }
+        catch (SQLException e) {
+            System.out.println("Что то пошло нет так" + e);
+        }
+
         finally {
+
             connect.close();
 
         }
 
     }
 }
-
