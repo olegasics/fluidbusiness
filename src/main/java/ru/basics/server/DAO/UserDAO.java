7419aaafa338bf822f5de75c3c2e78a75cd57f11
oleg <asicscwcs@gmail.com>
2019-09-14 13:16:16 +0300

    add maven hibernate

--- src/main/java/ru/basics/server/DAO/UserDAO.java
+++ src/main/java/ru/basics/server/DAO/UserDAO.java
@@ -1,7 +1,12 @@
 package ru.basics.server.DAO;
 import ru.basics.server.User;
 
+import org.hibernate.cfg.Configuration;
+import org.hibernate.SessionFactory;
+
 public class UserDAO implements DAO<User, String> {
+
+    private final SessionFactory sessionFactory = null;
 
     @Override
     public void create(User user) {
