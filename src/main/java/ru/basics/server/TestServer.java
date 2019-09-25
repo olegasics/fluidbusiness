44b2f7a970396af853c49f0c44608ec60a79bbe3
Oleg <basicscode@yandex.ru>
2019-09-25 13:47:48 +0300

    small change

--- src/main/java/ru/basics/server/TestServer.java
+++ src/main/java/ru/basics/server/TestServer.java
@@ -5,7 +5,7 @@ public class TestServer {
     public static void main(String[] args) {
      Registration reg = new Registration();
 //        reg.newAddUser("Олег", 1, "Масло", "omaslo", "edcvfr12!", "89038456869", "Москва");
-        reg.newAddUser("Елена", 2, "Алексеева", "ealekseeva", "edcvfr12!", "89855661313", "Москва");
+        reg.newAddUser("Елена", 2, "ealekseeva", "edcvfr12!", "89855661313", "Москва");
         Auth auth = new Auth();
        //auth.Authy("omaslo");
         System.out.println(auth.Authy("omaslo"));
