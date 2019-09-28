5039cdeb856126ea88c362439aa9477235fdc750
oleg <asicscwcs@gmail.com>
2019-09-28 23:59:38 +0300

    AddNewCargo add

---
+++ src/main/java/ru/basics/server/entity/cargo/cargoMove/AddNewCargo.java
@@ -0,0 +1,13 @@
+package ru.basics.server.entity.cargo.cargoMove;
+
+import com.sun.istack.NotNull;
+import org.hibernate.SessionFactory;
+
+public class AddNewCargo {
+    SessionFactory sessionFactory = null;
+    CargoMove cargoMove = null;
+
+    public void add(String status, String RP) {
+
+    }
+}
