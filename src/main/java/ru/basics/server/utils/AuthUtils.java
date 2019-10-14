9e6a53f526e8e57f8874fc09b83371345f021f98
Oleg <basicscode@yandex.ru>
2019-10-14 15:37:22 +0300

    refsctoring AuthUtils

--- src/main/java/ru/basics/server/utils/AuthUtils.java
+++ src/main/java/ru/basics/server/utils/AuthUtils.java
@@ -7,9 +7,6 @@ import ru.basics.server.database.exceptions.UserNotFoundException;
 
 public class AuthUtils {
     private static final UserDAO userDAO = new UserDAO();
-    private static String userPasswordInFiled;
-    private static String passwordInDB;
-    private static String userLoginInFiled;
 
     public static User signUp(User user) throws UserAlreadyExistsException {
         if (!userDAO.exists(user.getLogin())) {
@@ -20,10 +17,12 @@ public class AuthUtils {
     }
 
     public static String signIn(User user) throws UserNotFoundException {
-        userLoginInFiled = user.getLogin();
+        String userLoginInFiled = user.getLogin();
+
         if (userDAO.exists(userLoginInFiled)) {
-            userPasswordInFiled = user.getPassword();
-            passwordInDB = userDAO.findByField("login", userLoginInFiled).get(0).getPassword();
+            String userPasswordInFiled = user.getPassword();
+            String passwordInDB = userDAO.findByField("login", userLoginInFiled).get(0).getPassword();
+
             if (userPasswordInFiled.equals(passwordInDB)) {
                 return "AUTH TOKEN"; // TODO Почитай про это
             } else {
